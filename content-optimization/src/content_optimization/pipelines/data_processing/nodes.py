6b0bff25da33510db27c4a35c8ea3a23d62f7d2a
issacj-17 <68261814+issacj-17@users.noreply.github.com>
2024-06-27 09:30:03 +0800

    Truncated filname for .txt files to support Windows filenaming convention
    - Truncated title to 25 characters and appended article id to filename
    - Added uuid key to parameters_data_processing.yml to allow for edits to column via config file
    Note: Title was truncated to 25 characters instead of 10 due to many articles sharing the same 10 characters. Increasing to 25 characters support an easier visual inspection of .txt files to cross-reference with HealthHub articles online.

--- content-optimization/src/content_optimization/pipelines/data_processing/nodes.py
+++ content-optimization/src/content_optimization/pipelines/data_processing/nodes.py
@@ -121,6 +121,7 @@ def extract_data(
             # Load the dataframe
             df = partition_load_func()
 
+            uuid = metadata[content_category]["uuid"]
             content_title = metadata[content_category]["content_title"]
             content_body = metadata[content_category]["content_body"]
 
@@ -160,6 +161,10 @@ def extract_data(
 
                 # Substitute forbidden characters for filenames with _
                 title = re.sub(r'[<>:"/\\|?*]', "_", title)
+
+                # Truncate title to 10 characters and append the id
+                title = title[:25] + f"_{row[uuid]}"
+                print(title)
 
                 # Store text files in its own folder named `content_category`
                 all_extracted_text[os.path.join(content_category, title)] = (
