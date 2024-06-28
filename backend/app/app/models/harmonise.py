3e475b21dcdf4a61a82e95abaa46d833cd758a21
Tsien Jin <tsien.jin@gmail.com>
2024-06-28 10:19:51 +0800

    Backend --  Added response models
    - Added model for API response type for job types

--- backend/app/app/models/harmonise.py
+++ backend/app/app/models/harmonise.py
@@ -2,8 +2,19 @@ from typing import List
 
 from pydantic import BaseModel, Field
 
+from app.models.article import Article
+
 
 class Harmonise(BaseModel):
     id: str = Field(default='')  # Will only be present when retrieving from DB
     name: str = Field()
     article_ids: List[str] = Field(default=[])
+
+
+class HarmonisePopulated(BaseModel):
+    """
+    This model will be the response type for frontend consumption
+    """
+    id: str = Field()
+    name: str = Field()
+    articles: Article
--- backend/app/app/models/optimise.py
+++ backend/app/app/models/optimise.py
@@ -1,6 +1,16 @@
 from pydantic import BaseModel, Field
 
+from app.models.article import Article
+
 
 class Optimise(BaseModel):
     id: str = Field(default='')
     article_id: str = Field()
+
+
+class OptimisePopulated(BaseModel):
+    """
+    This model will be the response type for frontend consumption
+    """
+    id: str = Field()
+    article: Article
