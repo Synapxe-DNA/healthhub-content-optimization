6e48c45165ea5a131f4583fd408b3d010e6fec1b
Tsien Jin <tsien.jin@gmail.com>
2024-07-01 12:17:58 +0800

    Ran linting

--- backend/app/app/utils/db_connector/mongo_connector/mongo_connector.py
+++ backend/app/app/utils/db_connector/mongo_connector/mongo_connector.py
@@ -1,16 +1,19 @@
-import asyncio
 from typing import List
-
-from beanie import init_beanie
-from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 
 from app.interfaces.db_connector_types import DbConnector
 from app.models.article import Article
 from app.models.cluster import Cluster
 from app.models.harmonise import Harmonise
 from app.models.optimise import Optimise
-from app.utils.db_connector.mongo_connector.beanie_documents import ClusterDocument, ArticleDocument, EdgeDocument, \
-    HarmoniseDocument, OptimiseDocument
+from app.utils.db_connector.mongo_connector.beanie_documents import (
+    ArticleDocument,
+    ClusterDocument,
+    EdgeDocument,
+    HarmoniseDocument,
+    OptimiseDocument,
+)
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 
 
 class MongoConnector(DbConnector):
@@ -18,25 +21,21 @@ class MongoConnector(DbConnector):
     Class to connect to Mongo DB Instance
     """
 
-    __connection_string:str
-    __db:str
-    __client:AsyncIOMotorClient
-    __conn:AsyncIOMotorDatabase
-
+    __connection_string: str
+    __db: str
+    __client: AsyncIOMotorClient
+    __conn: AsyncIOMotorDatabase
 
     """
     Client setup
     """
 
     def __init__(
-            self,
-            username:str,
-            password:str,
-            host:str,
-            port:str,
-            db_name:str
+        self, username: str, password: str, host: str, port: str, db_name: str
     ):
-        assert username and password and host and port and db_name, "Required params not provided!"
+        assert (
+            username and password and host and port and db_name
+        ), "Required params not provided!"
 
         self.__connection_string = f"mongodb://{username}:{password}@{host}:{port}/"
         self.__db = db_name
@@ -52,10 +51,9 @@ class MongoConnector(DbConnector):
                 ArticleDocument,
                 EdgeDocument,
                 HarmoniseDocument,
-                OptimiseDocument
-            ]
+                OptimiseDocument,
+            ],
         )
-
 
     """
     Class methods to interact with DB
@@ -67,14 +65,10 @@ class MongoConnector(DbConnector):
                 name=c.name,
                 article_ids=c.article_ids,
                 edges=[
-                    EdgeDocument(
-                        start=e.start,
-                        end=e.end,
-                        weight=e.weight
-                    ) for e in c.edges
-                ]
+                    EdgeDocument(start=e.start, end=e.end, weight=e.weight)
+                    for e in c.edges
+                ],
             ).create()
-
 
     async def read_cluster_all(self) -> List[Cluster]:
         pass
@@ -94,7 +88,7 @@ class MongoConnector(DbConnector):
                 labels=a.labels,
                 cover_image_url=a.cover_image_url,
                 engagement=a.engagement,
-                views=a.views
+                views=a.views,
             ).create()
 
     async def read_article_all(self) -> List[Article]:
@@ -105,10 +99,7 @@ class MongoConnector(DbConnector):
 
     async def create_harmonise(self, harmonisation: List[Harmonise]):
         for h in harmonisation:
-            await HarmoniseDocument(
-                name=h.name,
-                article_ids=h.article_ids
-            ).create()
+            await HarmoniseDocument(name=h.name, article_ids=h.article_ids).create()
 
     async def read_harmonise_all(self) -> List[Harmonise]:
         pass
@@ -118,9 +109,7 @@ class MongoConnector(DbConnector):
 
     async def create_optimise(self, optimisation: List[Optimise]):
         for o in optimisation:
-            await OptimiseDocument(
-                article_id=o.article_id
-            ).create()
+            await OptimiseDocument(article_id=o.article_id).create()
 
     async def read_optimise_all(self) -> List[Optimise]:
         pass
