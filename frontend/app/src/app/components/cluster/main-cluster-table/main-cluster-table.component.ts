38c43f852213f40ce369a392756cdb0c428da857
Lyndon Lim <hnghng127@gmail.com>
2024-07-03 11:52:13 +0800

    Change table to Taiga UI table

--- frontend/app/src/app/components/cluster/main-cluster-table/main-cluster-table.component.ts
+++ frontend/app/src/app/components/cluster/main-cluster-table/main-cluster-table.component.ts
@@ -1,28 +1,31 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy,Component, Input, ViewChild } from '@angular/core';
 import { Cluster } from '../../../pages/clusters/clusters.component';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { TuiRadioBlockModule } from '@taiga-ui/kit';
-import { TuiGroupModule } from '@taiga-ui/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { TuiDataListWrapperModule, TuiInputNumberModule, TuiRadioBlockModule, TuiTextareaModule } from '@taiga-ui/kit';
+import { TuiFormatNumberPipeModule, TuiGroupModule, TuiScrollbarModule, TuiSvgModule, tuiFormatNumber } from '@taiga-ui/core';
+import { TuiComparator, TuiTableModule } from '@taiga-ui/addon-table';
+import { TuiDay, TuiLetModule, TuiValidatorModule, tuiDefaultSort } from '@taiga-ui/cdk';
+
 
 @Component({
   selector: 'app-main-cluster-table',
   standalone: true,
-  imports: [FormsModule,ReactiveFormsModule,TuiRadioBlockModule, TuiGroupModule],
+  imports: [TuiLetModule,TuiFormatNumberPipeModule,FormsModule,ReactiveFormsModule,TuiRadioBlockModule, TuiGroupModule,TuiDataListWrapperModule, TuiTableModule, TuiScrollbarModule, TuiTextareaModule, TuiInputNumberModule,TuiValidatorModule, TuiSvgModule],
   templateUrl: './main-cluster-table.component.html',
-  styleUrl: './main-cluster-table.component.css'
+  styleUrl: './main-cluster-table.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
+
 export class MainClusterTableComponent {
   @Input() clusters: Cluster[] = [];
-  @Input() filter: {} = {};
-  selectedCluster: Number[] = [];
+ 
+    readonly columns = ['articleId', 'URL', 'pillar', 'status'] as const;
 
-  clusterSelected:FormControl = new FormControl('',Validators.required);
-
-  constructor() {
-  }
-
-
-  getCheckboxValues() {
-    console.log("Cluster select:" + this.clusterSelected.value)
-  }
+    clusterSelected:FormControl = new FormControl('',Validators.required);
+    
+    trackByIndex(index: number): number {
+        return index;
+    }
+ 
+ 
 }
