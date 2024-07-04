25f63637d55c97866c59c43780d12c9a7d6bfab6
sixiannn <sixiannn@gmail.com>
2024-07-04 16:50:35 +0800

    Added populate_mongo to populate real data in mongodb

---
+++ backend/app/app/scripts/populate_mongo/beanie_documents.py
@@ -0,0 +1,28 @@
+from typing import List
+from beanie import Document, Indexed, Link
+from pydantic import Field
+
+class ArticleDocument(Document):
+    title: str
+    description: str
+    author: Indexed(str)
+    pillar: Indexed(str)
+    url: str
+
+    status: str = Field(default="")
+
+    labels: List[str]
+    cover_image_url: str
+
+    engagement: float
+    views: int
+
+class EdgeDocument(Document):
+    start: Link[ArticleDocument]
+    end: Link[ArticleDocument]
+    weight: float
+
+class ClusterDocument(Document):
+    name: str
+    article_ids: List[Link[ArticleDocument]] = Field(default=[])
+    edges: List[Link[EdgeDocument]] = Field(default=[])
---
+++ backend/app/app/scripts/populate_mongo/main.py
@@ -0,0 +1,31 @@
+import asyncio
+import os
+from dotenv import load_dotenv
+from app.scripts.populate_mongo.db_populator import DBPopulator
+from app.utils.db_connector.mongo_connector.mongo_connector import MongoConnector
+
+load_dotenv("./.env")
+
+async def __main():
+    conn = MongoConnector(
+        username=os.getenv('MONGO_USERNAME'),
+        password=os.getenv('MONGO_PASSWORD'),
+        host=os.getenv('MONGO_HOST'),
+        port=os.getenv('MONGO_PORT'),
+        db_name="storage"
+    )
+
+    articles_file_path = os.path.join('app','data','merged_data.parquet')
+    edges_file_path = os.path.join('app','data','all-mpnet-base-v2_neo4j_edges.pkl')   #To be edited
+    cluster_file_path = os.path.join('app','data','all-mpnet-base-v2_neo4j_pred_cluster.pkl') #To be edited
+    db_populator = DBPopulator(conn, articles_file_path, edges_file_path, cluster_file_path)
+
+    await db_populator.populate_articles()
+    await db_populator.populate_edges()
+    await db_populator.populate_clusters()
+
+def main():
+    asyncio.run(__main())
+
+if __name__ == "__main__":
+    main()
