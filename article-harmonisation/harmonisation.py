c8697a14fc607082b8c231df46e63299a345bc6d
Jin Chou <chuajinchou@gmail.com>
2024-07-09 14:03:45 +0800

    compiler node

--- article-harmonisation/harmonisation.py
+++ article-harmonisation/harmonisation.py
@@ -1,123 +1,102 @@
-import json
 import os
-from langchain.tools.render import format_tool_to_openai_function
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langgraph.graph import END, StateGraph
-from langgraph.prebuilt.tool_executor import ToolExecutor, ToolInvocation
-from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.tracers.context import tracing_v2_enabled
-from langsmith import Client
 from dotenv import load_dotenv
-from models import Llama
-from langchain_huggingface import HuggingFaceEndpoint
 from typing import TypedDict, Optional
 from langgraph.graph import StateGraph
-from prompt import LLMPrompts
-
-article = """
-        Breast cancer is the number one cancer among women in Singapore. You can protect yourself by taking active preventive measures to protect yourself against them. Besides being well informed about these conditions, you can protect yourself by going for regular health screenings.
-
-        Breast Cancer Screening
-        Beyond the recommended monthly breast self-examinations, the best way to protect yourself from breast cancer is to go for regular mammograms. A mammogram can detect tiny lumps that cannot be felt by the hand. Early detection, followed by treatment and good control of the condition can result in better chances of surviving this cancer, lowering the risk of serious complications.
-        Women who are 50 years old and above are recommended to go for a mammogram once every two years. Women 40 to 49, can screen for breast cancer, annually, provided their doctor has discussed the benefits and limitations with them. It is important to make an informed choice about going for screening.
-        Related: Cancer Facts You Cannot Ignore
-
-        Subsidy for Mammograms
-        Under the Health Promotion Boards (HPB) Screen for Life (SFL)[1], women aged 50 years and above can benefit from subsidised mammogram screenings which cost $50 for Singapore citizens, $75 for Permanent Residents (PRs), $37.50 for Merdeka Generation Cardholders and $25 for Pioneer Generation Cardholders at participating locations.
-        Eligibility for subsidised mammograms under HPBs Screen for Life is as follows:
-        [1]Screen for Life refers to HPBs screening programmes for cardiovascular diseases and selected cancers. For more information, please visit Screen for Life..
-
-        Tips on Breast Cancer Screening Subsidies
-        If you are 50 years old and above, you can use your Medisave or the Medisave account of an immediate family member for your mammogram at Medisave-approved centres, including participating centres under HPBs Screen for Life that are listed in this article. The full list of approved centres can be found on the Ministry of Health's (MOH) website.
-        You can use up to $500 per Medisave account a year under the Medisave500 scheme.
-        The Singapore Cancer Society offers free mammography services at $0 for women aged 50 years and above, who have not gone for this screening in the last two years and have a valid Blue or Orange Community Health Assist Scheme Card (CHAS) card, at their Multi-Service Centre in Bishan.
-        Related: Breast Cancer Screening Now Free for Eligible Singaporeans
-
-        Where to Go for Screenings
-        The following centres offer subsidised mammogram screening. You are encouraged to call ahead to make an appointment.
-
-        Singapore Cancer Society Clinic @ Bishan
-        For women with a valid Blue or Orange Community Health Assist Scheme (CHAS) card Tel: 1800 727 3333^
-
-        Polyclinics
-        Call 6275 6443 to make an appointment.
-        Alternatively, you may use their online form or through HealthHub.
-        Call 6370 6556 to make an appointment.
-        Alternatively, you may use their online form or email nuhsd_contact@nuhs.edu.sg
-        Call 6536 6000 to make an appointment.
-        Alternatively, you may use their online form.
-
-        Useful Links
-        1. Understanding Breast Cancer Learn more about breast cancer, its risk factors, treatment options and mammogram procedures.
-        2. Breast Cancer Foundation / Singapore Cancer Society All matters relating to breast cancer, from mammogram procedures to public education programmes.
-        Learn more about breast cancer, its risk factors, treatment options and mammogram procedures.
-        All matters relating to breast cancer, from mammogram procedures to public education programmes.
-
-        Contact Information
-
-        Screen for Life
-        Telephone: 1800 223 1313^ Email: HPB_Mailbox@hpb.gov.sg
-        """
+from models import start_llm
 
 load_dotenv()
 
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY", "")
 os.environ["HUGGINGFACEHUB_API_TOKEN"]= os.getenv("HUGGINGFACEHUB_API_TOKEN", "")
 
-MODEL = "meta-llama/Meta-Llama-3-8B-Instruct"
+MODELS = ["Mistral", "Llama3"]
+MODEL = MODELS[0]
+NODE_ROLES = ['Researcher', "Comparer"]
 MAX_NEW_TOKENS = 3000
-NODE_ROLES = ['Researcher', "Compiler"]
 
+ROOT = os.getcwd()
+EXTRACTED_TEXT_DIRECTORY = f"{ROOT}/content-optimization/data/02_intermediate/all_extracted_text/"
+ARTICLE_CATEGORY = "diseases-and-conditions/"
+ARTICLE1_TITLE = "Diabetic Foot Ulcer_ Symp_1437648.txt"
+ARTICLE2_TITLE = "Diabetic Foot Care_1437355.txt"
 
-prompter = LLMPrompts()
 
 class GraphState(TypedDict):
-    article: Optional[str] = None
-    keypoints: Optional[str] = None
-    response: Optional[str] = None
+    articles: Optional[list]
+    keypoints: Optional[list]
+    response: Optional[str]
+    counter: Optional[int]
     
 
 workflow = StateGraph(GraphState)
 
 def researcher_node(state):
-    llm = HuggingFaceEndpoint(endpoint_url=MODEL, max_new_tokens=MAX_NEW_TOKENS)
-    comparer_agent = Llama(llm, prompter.return_researcher_prompt(), NODE_ROLES[0])
-    article = state.get('article', '').strip()
-    keypoints = comparer_agent.generate_text(article)
-    return {"keypoints": keypoints}
+    article_list = state.get('articles', '')
+    counter = state.get('counter', 0)
+    print("This is article ", counter+1)
+    keypoints = state.get('keypoints', [])
+    article = article_list[counter].strip()
+    article_keypoints = researcher_agent.generate_text(article)
+    new_article_keypoints = article_keypoints
+    keypoints.append(new_article_keypoints)
+    return {
+            "keypoints": keypoints,
+            "counter": counter + 1
+            }
 
-# def handle_greeting_node(state):
-#     return {"response": "Hello! How can I help you today?"}
-
-# def handle_search_node(state):
-#     question = state.get('question', '').strip()
-#     search_result = f"Search result for '{question}'"
-#     return {"response": search_result}
-
+def compiler_node(state):
+    keypoints = state.get("keypoints")
+    print("this is keypoints", len(keypoints))
+    compiled_keypoints = comparer_agent.compile_points(keypoints)
+    return {
+            "response": compiled_keypoints
+            }
 
 workflow.add_node("get_keypoints", researcher_node)
-# workflow.add_node("handle_greeting", handle_greeting_node)
-# workflow.add_node("handle_search", handle_search_node)
+workflow.add_node("compile_keypoints", compiler_node)
 
-# def decide_next_node(state):
-#     return "handle_greeting" if state.get('classification') == "greeting" else "handle_search"
+def decide_next_node(state):
+    return "get_keypoints" if state.get('counter') < article_list_length else "compile_keypoints"
 
-# workflow.add_conditional_edges(
-#     "classify_input",
-#     decide_next_node,
-#     {
-#         "handle_greeting": "handle_greeting",
-#         "handle_search": "handle_search"
-#     }
-# )
+workflow.add_conditional_edges(
+    "get_keypoints",
+    decide_next_node,
+    {
+        "get_keypoints": "get_keypoints",
+        "compile_keypoints": "compile_keypoints"
+    }
+)
 
 workflow.set_entry_point("get_keypoints")
-
-workflow.add_edge('get_keypoints', END)
+workflow.add_edge('compile_keypoints', END)
 
 app = workflow.compile()
 
-inputs = {"article": article}
-result = app.invoke(inputs)
-print(result['keypoints'])
+researcher_agent = start_llm(MODEL, NODE_ROLES[0])
+comparer_agent = start_llm(MODEL, NODE_ROLES[1])
 
+#load the articles
+with open(f'{EXTRACTED_TEXT_DIRECTORY}{ARTICLE_CATEGORY}{ARTICLE1_TITLE}', 'r') as file:
+    article_1 = file.read()
+with open(f'{EXTRACTED_TEXT_DIRECTORY}{ARTICLE_CATEGORY}{ARTICLE2_TITLE}', 'r') as file:
+    article_2 = file.read()
+
+
+
+
+article_list = [article_1, article_2]
+article_list_length = len(article_list)
+
+inputs = {"articles": article_list,
+          "keypoints": [],
+          "counter": 0}
+
+result = app.invoke(inputs)
+for i in range(0,2):
+    print("THIS IS KEYPOINTS")
+    print(str(result['keypoints'][i]))
+    print("-----------------")
+print("THIS IS THE RESPONSE")
+print(str(result['response']))
--- article-harmonisation/models.py
+++ article-harmonisation/models.py
@@ -6,7 +6,8 @@ from abc import ABC, abstractmethod
 from langchain_core.prompts import PromptTemplate
 from langchain.chains.llm import LLMChain
 from langchain.chains.conversation.base import ConversationChain
-from prompt import LLMPrompts
+from langchain_core.output_parsers import StrOutputParser
+from prompt import prompt_tool
 
 
 dotenv.load_dotenv()
@@ -15,15 +16,31 @@ os.environ["HUGGINGFACEHUB_API_TOKEN"]= os.getenv("HUGGINGFACEHUB_API_TOKEN", ""
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_PROJECT"] = "Multi-agent Collaboration"
 
-MODEL_NAME = 'llama3'
-ROLES = ['Researcher', "Compiler"]
+MODELS = ["meta-llama/Meta-Llama-3-8B-Instruct", "microsoft/Phi-3-mini-128k-instruct", "mistralai/Mistral-7B-Instruct-v0.3"]
+MAX_NEW_TOKENS = 3000
 
-prompts = LLMPrompts()
-ans = prompts.return_researcher_prompt()
+def start_llm(model, role):
+    if model == "Mistral":
+        model_prompter = prompt_tool(model= model)
+        llm = HuggingFaceEndpoint(endpoint_url=MODELS[2], max_new_tokens=MAX_NEW_TOKENS)
+        return Mistral(llm, model_prompter, role)
+    elif model == "Llama3":
+        model_prompter = prompt_tool(model)
+        llm = HuggingFaceEndpoint(endpoint_url=MODELS[0], max_new_tokens=MAX_NEW_TOKENS)
+        return Llama(llm, model_prompter, role)
+    else:
+        return "Your model does not exist"
+    pass
+
+
 
 class LLMInterface(ABC):
     @abstractmethod
     def generate_text(self, question, context):
+        pass
+
+    @abstractmethod
+    def compile_points(self, keypoints):
         pass
 
 class Llama(LLMInterface):
@@ -32,29 +49,60 @@ class Llama(LLMInterface):
         self.prompt_template = prompt_template
         self.role = role
 
-    def generate_text(self, article = " "):
+    def generate_text(self, article : str):
         if self.role == "Researcher":
-            prompt_t = PromptTemplate.from_template(self.prompt_template)
-            chain = LLMChain(
-                llm=self.model,
-                prompt = prompt_t)        
+            prompt_t = PromptTemplate(input_variables=["Article"], template=self.prompt_template.return_researcher_prompt())
+            chain = prompt_t | self.model | StrOutputParser()
             print("generating keypoints")
-            response1 = chain.invoke({"Article": article})
+            response = chain.invoke(article)
             print("keypoints generated")
-            answer = response1['text']
+            answer = response['text']
             return answer
         else:
             return "This node cannot run generate_text()"
     
-    def generate_article(self, keypoints1, keypoints2):
-        if self.role == "Compiler":
-            prompt_t = PromptTemplate.from_template(self.prompt_template)
+    def compile_points(self, keypoints: list = []):
+        if self.role == "Comparer":
+            prompt_t = PromptTemplate.from_template(self.prompt_template.return_comparer_prompt())
             chain = LLMChain(
                 llm=self.model,
                 prompt = prompt_t)        
-            response1 = chain.invoke({"KP1": keypoints1, "KP2" : keypoints2})
+            response1 = chain.invoke({"Keypoints" : keypoints})
             answer = response1['text']
             return answer
         else:
             return "This node cannot run generate_article()"
-        
+
+class Mistral(LLMInterface):
+    def __init__(self, model, prompt_template, role):
+        self.model = model
+        self.prompt_template = prompt_template
+        self.role = role
+
+    def generate_text(self, article : str):
+        if self.role == "Researcher":
+            prompt_t = PromptTemplate(input_variables=["Article"], template=self.prompt_template.return_researcher_prompt())
+            chain = prompt_t | self.model | StrOutputParser()
+            print("generating keypoints")
+            response = chain.invoke(article)
+            print("keypoints generated")
+            return response
+        else:
+            return "This node cannot run generate_text()"
+    
+    def compile_points(self, keypoints: list = []):
+        if self.role == "Comparer":
+            prompt_t = PromptTemplate.from_template(self.prompt_template.return_compiler_prompt())
+            if len(keypoints) >= 2:
+                input_keypoints = "--START OF KEYPOINTS--"
+                for index in range(len(keypoints)):
+                    input_keypoints += f"\n Article {index + 1} keypoints:\n"
+                    input_keypoints += f"\n {keypoints[index]}\n"
+                input_keypoints += "\n --END OF KEYPOINTS--"
+                chain = prompt_t | self.model
+                response = chain.invoke({"Keypoints" : input_keypoints})
+                return response
+            else:
+                return "There are insufficient number of keypoints to compare. Kindly add more articles to compare and try again."
+        else:
+            return "This node cannot run generate_article()"
