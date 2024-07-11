f23f5dc9e7074eabba416f01883afe4e2288d9a9
sixiannn <sixiannn@gmail.com>
2024-07-11 16:17:06 +0800

    Merge feature_engineering

--- content-optimization/src/content_optimization/pipelines/feature_engineering/pipeline.py
+++ content-optimization/src/content_optimization/pipelines/feature_engineering/pipeline.py
@@ -3,7 +3,11 @@ This is a boilerplate pipeline 'feature_engineering'
 generated using Kedro 0.19.6
 """
 
-from content_optimization.pipelines.feature_engineering.nodes import extract_keywords
+from content_optimization.pipelines.feature_engineering.nodes import (
+    combine_embeddings_by_weightage,
+    extract_keywords,
+    generate_embeddings,
+)
 from kedro.pipeline import Pipeline, node, pipeline
 
 
@@ -27,6 +31,33 @@ def create_pipeline(**kwargs) -> Pipeline:
                 ],
                 outputs="filtered_data_with_keywords",
                 name="extract_keywords_node",
+            ),
+            node(
+                func=generate_embeddings,
+                inputs=[
+                    "filtered_data_with_keywords",
+                    "params:embeddings.model",
+                    "params:embeddings.owner",
+                    "params:embeddings.trust_remote_code",
+                    "params:embeddings.pooling_strategy",
+                    "params:columns_to_keep_emb",
+                    "params:columns_to_emb",
+                ],
+                outputs="embeddings_data",
+                name="generate_embeddings_node",
+            ),
+            node(
+                func=combine_embeddings_by_weightage,
+                inputs=[
+                    "embeddings_data",
+                    "params:embeddings_weightage.title",
+                    "params:embeddings_weightage.article_category_names",
+                    "params:embeddings_weightage.category_description",
+                    "params:embeddings_weightage.extracted_content_body",
+                    "params:embeddings_weightage.keywords",
+                ],
+                outputs="weighted_embeddings",
+                name="combine_embeddings_by_weightage",
             ),
         ]
     )
