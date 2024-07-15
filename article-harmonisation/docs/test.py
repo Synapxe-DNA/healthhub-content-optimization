20632e46aba00fcfc7ab819b2d7415b43185b751
Jin Chou <chuajinchou@gmail.com>
2024-07-15 14:29:20 +0800

    finished lint

--- article-harmonisation/docs/test.py
+++ article-harmonisation/docs/test.py
@@ -1,6 +1,9 @@
 import sys
+
 sys.path.append("content-optimization/src/content_optimization/utils/")
 
 from extract import HTMLExtractor
 
-HTMLExtractor("https://www.healthhub.sg/a-z/costs-and-financing/breast-cancer-screening-subsidies")
+HTMLExtractor(
+    "https://www.healthhub.sg/a-z/costs-and-financing/breast-cancer-screening-subsidies"
+)
