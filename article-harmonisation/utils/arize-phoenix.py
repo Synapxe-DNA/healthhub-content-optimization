4804c8b93cbc95fe1ed419da200272f76f479e58
issacj-17 <68261814+issacj-17@users.noreply.github.com>
2024-07-21 11:26:31 +0800

    Update arize-phoenix.py
    - Formatted function to meet linting requirements
    - Amended function to test multiple keys in dataset

--- article-harmonisation/utils/arize-phoenix.py
+++ article-harmonisation/utils/arize-phoenix.py
@@ -1,9 +1,12 @@
-import requests
 import pandas as pd
 import phoenix as px
 
 
-def upload_parquet(dataset_name: str, parquet_filepath: str = "../data/merged_data.parquet", upload_url: str = "http://127.0.0.1:6006"):
+def upload_parquet(
+    dataset_name: str,
+    parquet_filepath: str = "../data/merged_data.parquet",
+    upload_url: str = "http://127.0.0.1:6006",
+):
     client = px.Client(endpoint=upload_url)
 
     df = pd.read_parquet(parquet_filepath)
@@ -11,7 +14,7 @@ def upload_parquet(dataset_name: str, parquet_filepath: str = "../data/merged_da
     dataset = client.upload_dataset(
         dataframe=df,
         dataset_name=dataset_name,
-        input_keys=["content_body"],
+        input_keys=["content_body", "content_category"],
         output_keys=["extracted_content_body"],
     )
 
@@ -22,4 +25,4 @@ if __name__ == "__main__":
     # parquet_filepath = sys.argv[1]
     # upload_url = sys.argv[2]
     # print(upload_dataset(upload_url, parquet_filepath))
-    print(upload_parquet("test123"))
+    print(upload_parquet("test1234"))
