d6331094687165fdedc794d8f67c561b229bfe7f
Jin Chou <chuajinchou@gmail.com>
2024-08-28 10:02:27 +0800

    excel_extractor doc strings: :

--- article-harmonisation/utils/excel_extractor.py
+++ article-harmonisation/utils/excel_extractor.py
@@ -3,10 +3,22 @@ import warnings
 from openpyxl import load_workbook
 from states.definitions import OptimisationFlags
 
+# Filters out warnings
 warnings.simplefilter(action="ignore", category=UserWarning)
 
 
-def return_optimisation_flags(article, rewriting_process):
+def return_optimisation_flags(article, rewriting_process: str):
+    """
+    Returns the user flags for article rewriting. Article harmonisation will return all flags as True while article optimisation will be dependent on the user flags in the User Annotation Excel File
+
+    Args:
+        article (DataFrame): A DataFrame consisting of articles from the User Annotation Excel file.
+        rewriting_process (str): A String indicating which rewriting process. It can either be "optimisation" or "harmonisation"
+
+    Returns:
+        flags (OptimisationFlags): A TypedDict with keys for each optimisation flag. The flags will either be True or False, depending on the user input in the User Annotation Excel file
+
+    """
     flags = OptimisationFlags(
         flag_for_content_optimisation=False,
         flag_for_title_optimisation=True,
@@ -37,7 +49,16 @@ def return_optimisation_flags(article, rewriting_process):
             return flags
 
 
-def store_optimised_outputs(file_path, sheet_name, article_data):
+def store_optimised_outputs(file_path: str, sheet_name: str, article_data: str):
+    """
+    Stores the optimised outputs into an Excel file.
+
+    Args:
+        file_path (str): A String indicating the file path to the destination Excel file
+        sheet_name (str): A String indicating the name of the sheet to store the output in
+        article_data (str): A String storing the article data to be stored
+
+    """
     try:
         # Load the existing workbook
         workbook = load_workbook(file_path)
