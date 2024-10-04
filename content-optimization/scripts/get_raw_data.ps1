c6a785138487f636ff7ae19bbaae07e413c2ef6f
Wilsven Leong <wilsven_leong96@hotmail.co.uk>
2024-10-04 15:23:24 +0800

    Updated scripts for DVC

--- content-optimization/scripts/get_raw_data.ps1
+++ content-optimization/scripts/get_raw_data.ps1
@@ -1,3 +1,11 @@
-. ./scripts/setup_dvc.ps1
+# Define arrays for file paths and their corresponding remotes
+$filePaths = @("data/01_raw/all_contents.dvc", "data/01_raw/missing_contents.dvc", "data/01_raw/google_analytics.xlsx.dvc")
+$remotes = @("all_contents", "missing_contents", "google_analytics")
 
-dvc pull
+# Loop through each file and remote using their indices
+for ($i = 0; $i -lt $filePaths.Length; $i++) {
+    $filePath = $filePaths[$i]
+    $remote = $remotes[$i]
+    Write-Host "Pulling $filePath from remote $remote..."
+    & dvc pull $filePath --remote $remote
+}
